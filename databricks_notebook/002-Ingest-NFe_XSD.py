7ad4fec48b81e638623ceb764ce545e4f0d749b4
lcpassuncao <lcpassuncao>
2024-04-12 05:11:59 +0000

    databricks notebook

--- databricks_notebook/002-Ingest-NFe_XSD.py
+++ databricks_notebook/002-Ingest-NFe_XSD.py
@@ -1,17 +1,14 @@
 # Databricks notebook source
 # MAGIC %md-sandbox
-# MAGIC 
-# MAGIC <div style="text-align: left; valign: top; line-height: 0; padding-top: 9px;">
-# MAGIC   <img src="https://raw.githubusercontent.com/Databricks-BR/Databricks-BR/main/images/databricks_br_header.png" alt="GitHub Databricks Brasil" style="width: 700px"><a href="https://github.com/Databricks-BR"><img src="https://raw.githubusercontent.com/Databricks-BR/Databricks-BR/main/images/icon_github.png" style="width: 40px; height: 40px;"></a>
-# MAGIC    <a href="https://github.com/Databricks-BR"><img src="https://raw.githubusercontent.com/Databricks-BR/Databricks-BR/main/images/databricks-br.png" style="width: 40px; height: 40px;"></a>  <a href="https://www.linkedin.com/groups/14100135"><img src="https://raw.githubusercontent.com/Databricks-BR/Databricks-BR/main/images/icon_linkedin.png" style="width: 35px; height: 35px;"></a>  <a href="https://www.meetup.com/pt-BR/databricks-brasil-oficial"><img src="https://raw.githubusercontent.com/Databricks-BR/Databricks-BR/main/images/icon_meetup.png" style="height: 40px;"></a>  <a href="https://bit.ly/databricks-slack-br"><img src="https://raw.githubusercontent.com/Databricks-BR/Databricks-BR/main/images/icon_slack.png" style="width: 35px; height: 35px;"></a>  <a href="https://www.youtube.com/channel/UCH3cq9mit-0UkTu1mTki20Q"><img src="https://raw.githubusercontent.com/Databricks-BR/Databricks-BR/main/images/icon_youtube.png" style="height: 38px;"></a>
-# MAGIC </div>
+# MAGIC
+# MAGIC <img src="https://raw.githubusercontent.com/Databricks-BR/open_tax/main/images/header_opentax.png" width="800px">
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ### Ingestão Nota Fiscal Eletrônica - NFe - XSD - Schema
 # MAGIC </br>
-# MAGIC 
+# MAGIC
 # MAGIC ##### Referência técnica de leitura do XSD:
 # MAGIC * https://learn.microsoft.com/pt-br/azure/databricks/data/data-sources/xml#xsd-support
 
@@ -19,19 +16,19 @@
 
 # DBTITLE 1,Cópia dos arquivos de origem (do GitHub)
 # MAGIC %scala
-# MAGIC 
+# MAGIC
 # MAGIC // Faz a copia dos arquivos TESTE a serem carregados (Origem:  Github.com/Databricks-BR).
 # MAGIC // Esse passo, em produção, deve ser substituído para fazer a ingestão direto
 # MAGIC // no repositório das Notas Fiscais da empresa
-# MAGIC 
+# MAGIC
 # MAGIC // bibliotecas
 # MAGIC import java.net.URL
 # MAGIC import java.io.File
 # MAGIC import org.apache.commons.io.FileUtils
-# MAGIC 
+# MAGIC
 # MAGIC // Cria um subdiretorio temporario para cópia
 # MAGIC dbutils.fs.mkdirs("/tmp/tax_lakehouse")
-# MAGIC 
+# MAGIC
 # MAGIC // inicializacao de variaveis (constantes)
 # MAGIC // localizacao dos arquivos de entrada e saida
 # MAGIC var github = "https://raw.githubusercontent.com/Databricks-BR/tax_lakehouse/main/datasets/"
@@ -39,19 +36,19 @@
 # MAGIC var tmpPath = "/tmp/tax_lakehouse/"
 # MAGIC var tmpFileXML = tmpPath + "nfe.xml"
 # MAGIC var dbfsFileXML = working_dir + "nfe.xml"
-# MAGIC 
+# MAGIC
 # MAGIC // faz a copia do GitHub de origem para um diretorio temporario 
 # MAGIC FileUtils.copyURLToFile(new URL(github + "NFe_XML/NFe_001.xml"), new File(tmpFileXML))
-# MAGIC 
+# MAGIC
 # MAGIC // faz a copia para o DBFS (Databricks File System)
 # MAGIC dbutils.fs.cp( "file://" + tmpFileXML, dbfsFileXML )
-# MAGIC 
+# MAGIC
 # MAGIC // faz a copia dos arquivos de estrutura do XML (schema)
 # MAGIC var fileXSD = new Array[String](3)
 # MAGIC fileXSD(0) = "leiauteNFe_v4.00.xsd"            // Layout principal da Nota Fiscal versao 4.00
 # MAGIC fileXSD(1) = "xmldsig-core-schema_v1.01.xsd"   // Layout chamado (import) dentro do layout principal
 # MAGIC fileXSD(2) = "tiposBasico_v4.00.xsd"           // Layout chamado (include) dentro do layout principal
-# MAGIC 
+# MAGIC
 # MAGIC for( iXSD <- 0 to 2){
 # MAGIC       FileUtils.copyURLToFile(new URL(github + "XSD_schema/" + fileXSD(iXSD) ), new File(tmpPath + fileXSD(iXSD) ));
 # MAGIC       dbutils.fs.cp( "file://" + tmpPath + fileXSD(iXSD) , working_dir + fileXSD(iXSD)  ) ;  
@@ -70,19 +67,18 @@
 # COMMAND ----------
 
 # DBTITLE 1,Verificação dos arquivos (passo de teste)
-# MAGIC %python
-# MAGIC 
-# MAGIC #dbutils.fs.ls("file:/tmp/tax_lakehouse/")                 # Lista os arquivos do diretorio Temporario
-# MAGIC # dbutils.fs.head("file:/tmp/tax_lakehouse/nfe.xml")       # leitura do conteudo inicial do arquivo
-# MAGIC 
-# MAGIC dbutils.fs.ls(f"dbfs:/user/luis_assuncao/tax_lakehouse/") # Lista os arquivos do Databricks File System
-# MAGIC # dbutils.fs.head("dbfs:/user/luis_assuncao/tax_lakehouse/nfe.xml")
+
+#dbutils.fs.ls("file:/tmp/tax_lakehouse/")                 # Lista os arquivos do diretorio Temporario
+# dbutils.fs.head("file:/tmp/tax_lakehouse/nfe.xml")       # leitura do conteudo inicial do arquivo
+
+dbutils.fs.ls(f"dbfs:/user/luis_assuncao/tax_lakehouse/") # Lista os arquivos do Databricks File System
+# dbutils.fs.head("dbfs:/user/luis_assuncao/tax_lakehouse/nfe.xml")
 
 # COMMAND ----------
 
 # DBTITLE 1,Bibliotecas de manipulação de XML e XSD
 # MAGIC %scala
-# MAGIC 
+# MAGIC
 # MAGIC import com.databricks.spark.xml._ 
 # MAGIC import com.databricks.spark.xml.functions.from_xml
 # MAGIC import com.databricks.spark.xml.schema_of_xml
@@ -94,25 +90,25 @@
 
 # DBTITLE 1,Leitura do Schema (layout do XML)
 # MAGIC %scala
-# MAGIC 
+# MAGIC
 # MAGIC // arquivo contendo o schema (estrutura aninhada do XML)
 # MAGIC var schema_name = working_dir + fileXSD(0)
-# MAGIC 
+# MAGIC
 # MAGIC // primeiro faz a leitura da estrutura do XML (Schema layout)
 # MAGIC val schema_xsd = XSDToSchema.read(Paths.get(schema_name))
-# MAGIC 
+# MAGIC
 # MAGIC println(schema_xsd)
 
 # COMMAND ----------
 
 # MAGIC %scala
-# MAGIC 
+# MAGIC
 # MAGIC // depois utiliza o "schema" lido para carregar a estrutura do XML
 # MAGIC val df = spark.read
 # MAGIC    .schema(schema_xsd)
 # MAGIC    .option("rowTag", "dest")
 # MAGIC    .xml(working_dir + "nfe.xml")
-# MAGIC 
+# MAGIC
 # MAGIC display(df)
 
 # COMMAND ----------
@@ -121,11 +117,11 @@
 # MAGIC val df = spark.read
 # MAGIC   .option("rowTag", "dest")
 # MAGIC   .xml(working_dir + "nfe.xml")
-# MAGIC 
+# MAGIC
 # MAGIC display(df)
 
 # COMMAND ----------
 
 # MAGIC %scala
-# MAGIC 
+# MAGIC
 # MAGIC df.printSchema()
