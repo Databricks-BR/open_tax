642026bf893d7a7bb1db7366f1080b1259b989f2
lcpassuncao <lcpassuncao>
2024-04-15 16:54:55 +0000

    Databricks notebook

--- databricks_notebook/102-Ingest-NFe_silver.py
+++ databricks_notebook/102-Ingest-NFe_silver.py
@@ -92,95 +92,183 @@
 
 # COMMAND ----------
 
+# DBTITLE 1,NFe - informações de identificação da nota
 # MAGIC %sql
-# MAGIC select _id as id_nfe,
-# MAGIC _versao as versao_nfe,
-# MAGIC ide.cUF as uf,
-# MAGIC ide.cNF as cod_chave_acesso,
-# MAGIC ide.natOp as cod_nat_operacao,
-# MAGIC ide.indPag as ind_forma_pag,
-# MAGIC ide.mod as cod_modelo_fiscal,
-# MAGIC ide.serie as num_serie,
-# MAGIC ide.nNF as num_doc_fiscal,
-# MAGIC ide.dhEmi as dt_emissao,
-# MAGIC ide.dhSaiEnt as dt_saida_entrada,
-# MAGIC ide.tpNF as tip_operacao,
-# MAGIC ide.cMunFG as cod_mun_fato_gerador,
-# MAGIC ide.tpImp as tpImp,
-# MAGIC ide.tpEmis as tpEmis,
-# MAGIC ide.cDV as cDV,
-# MAGIC ide.tpAmb as tpAmb,
-# MAGIC ide.finNFe as finNFe,
-# MAGIC ide.indFinal as indFinal,
-# MAGIC ide.indPres as indPres,
-# MAGIC emit.CNPJ as emit_cnpj,
-# MAGIC emit.xNome as emit_xnome,
-# MAGIC emit.xFant as emit_xfant
-# MAGIC --emit.enderEmit.xLgr as emit_end_logradouro,
-# MAGIC --emit.enderEmit.nro as emit_end_nro,
-# MAGIC --emit.enderEmit.xCpl as emit_end_compl,
-# MAGIC --emit.enderEmit.xBairro as emit_end_bairro,
-# MAGIC --emit.enderEmit.cMun as emit_end_cmun,
-# MAGIC --emit.enderEmit.xMun as emit_end_xmun,
-# MAGIC --emit.enderEmit.UF as emit_end_uf,
-# MAGIC --emit.enderEmit.CEP as emit_end_cep,
-# MAGIC --emit.enderEmit.cPais as emit_end_cpais,
-# MAGIC --emit.enderEmit.xPais as emit_end_xpais,
-# MAGIC --emit.fone as emit_end_fone,
-# MAGIC --emit.fone as emit_end_fone,
-# MAGIC --emit.IE as emit_insc_estadual,
-# MAGIC --emit.IEST as emit_insc_estadual_st
-# MAGIC from tax.bronze.nfe_xml2
+# MAGIC
+# MAGIC create or replace table tax.silver.nfe_ide 
+# MAGIC as 
+# MAGIC select 
+# MAGIC     _id as id_nfe,
+# MAGIC     _versao as versao_layout, 
+# MAGIC     ide.cUF as num_uf,
+# MAGIC     ide.cNF as cod_chave_acesso,
+# MAGIC     ide.natOp as cod_nat_operacao,
+# MAGIC     ide.indPag as ind_forma_pag,
+# MAGIC     ide.mod as cod_modelo_fiscal,
+# MAGIC     ide.serie as num_serie,
+# MAGIC     ide.nNF as num_doc_fiscal,
+# MAGIC     ide.dhEmi as dt_emissao,
+# MAGIC     ide.dhSaiEnt as dt_saida_entrada,
+# MAGIC     ide.tpNF as cod_tipo_operacao,
+# MAGIC     ide.cMunFG as cod_mun_fato_gerador,
+# MAGIC     ide.tpImp as cod_tipo_impressao_danfe,
+# MAGIC     ide.tpEmis as cod_tipo_emissao_nf,
+# MAGIC     ide.cDV as num_dv_chave_acesso,
+# MAGIC     ide.tpAmb as tip_ambiente,
+# MAGIC     ide.finNFe as cod_finalidade_nfe,
+# MAGIC     ide.indFinal as ind_oper_consumidor_final,
+# MAGIC     ide.indPres as ind_presencao_comprador
+# MAGIC     from 
+# MAGIC     tax.bronze.nfe_xml;
+# MAGIC
+
+# COMMAND ----------
+
+# DBTITLE 1,NFe Emitente
+# MAGIC %sql
+# MAGIC
+# MAGIC create or replace table tax.silver.nfe_emit
+# MAGIC as 
+# MAGIC select 
+# MAGIC     _id as id_nfe,
+# MAGIC     emit.CNPJ as emit_cnpj,
+# MAGIC     emit.xNome as emit_nome_emitente,
+# MAGIC     emit.xFant as emit_nome_fantasia,
+# MAGIC     emit.enderEmit.xLgr as emit_end_logradouro,
+# MAGIC     emit.enderEmit.nro as emit_end_nro,
+# MAGIC   --  emit.enderEmit.xCpl as emit_end_complemento,
+# MAGIC     emit.enderEmit.xBairro as emit_end_bairro,
+# MAGIC     emit.enderEmit.cMun as emit_end_cod_municipio,
+# MAGIC     emit.enderEmit.xMun as emit_end_municipio,
+# MAGIC     emit.enderEmit.UF as emit_end_uf,
+# MAGIC     emit.enderEmit.CEP as emit_end_cep,
+# MAGIC     emit.enderEmit.cPais as emit_end_cod_pais,
+# MAGIC     emit.enderEmit.xPais as emit_end_pais,
+# MAGIC     emit.enderEmit.fone as emit_end_fone,
+# MAGIC     emit.IE as emit_insc_estadual
+# MAGIC  --   iif(exists(emit.IEST), emit.IEST, null) as emit_insc_estadual_st,
+# MAGIC  --   emit.IM as emit_insc_municipal,
+# MAGIC  --   emit.CNAE as cod_cnae,
+# MAGIC  
+# MAGIC from 
+# MAGIC     tax.bronze.nfe_xml;
+# MAGIC
+
+# COMMAND ----------
+
+# DBTITLE 1,NFe Destinatário
+# MAGIC %sql
+# MAGIC
+# MAGIC create or replace table tax.silver.nfe_dest
+# MAGIC as 
+# MAGIC select 
+# MAGIC     _id as id_nfe,
+# MAGIC     dest.CNPJ as dest_cnpj,
+# MAGIC  --   dest.CPF as dest_cpf,
+# MAGIC     dest.xNome as dest_nome_destinatario,
+# MAGIC     dest.enderDest.xLgr as dest_end_logradouro,
+# MAGIC     dest.enderDest.nro as dest_end_num,
+# MAGIC     dest.enderDest.xCpl as dest_end_complemento,
+# MAGIC     dest.enderDest.xBairro as dest_end_bairro,
+# MAGIC     dest.enderDest.cMun as dest_end_cod_municipio,
+# MAGIC     dest.enderDest.xMun as dest_end_municipio,
+# MAGIC     dest.enderDest.UF as dest_end_uf,
+# MAGIC     dest.enderDest.CEP as dest_end_cep,
+# MAGIC     dest.enderDest.cPais as dest_end_cod_pais,
+# MAGIC     dest.enderDest.xPais as dest_end_pais,
+# MAGIC     dest.enderDest.fone as dest_end_fone,
+# MAGIC     dest.IE as dest_insc_estadual
+# MAGIC    -- dest.ISUF as dest_insc_suframa,
+# MAGIC    -- dest.IM as dest_insc_municipal
+# MAGIC from 
+# MAGIC     tax.bronze.nfe_xml;
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC
+# MAGIC %sql
+# MAGIC
+# MAGIC USE catalog tax;
+# MAGIC USE silver;
+# MAGIC
+# MAGIC COMMENT ON TABLE nfe_ide IS 'Nota Fiscal Eletrônica, Informações de IDENTIFICAÇÃO da Nfe - TAG de Produtos (ide)';
+# MAGIC COMMENT ON TABLE nfe_emit IS 'Nota Fiscal Eletrônica, Informações do EMISSOR (emitente) da Nfe - TAG de Produtos (emit)';
+# MAGIC COMMENT ON TABLE nfe_dest IS 'Nota Fiscal Eletrônica, Informações de DESTINATÁRIO da Nfe - TAG de Produtos (dest)';
+# MAGIC
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN id_nfe COMMENT 'Número da Nota Fiscal Eletrônica. Chave de cruzamento (FK)';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN id_nfe COMMENT 'Número da Nota Fiscal Eletrônica. Chave de cruzamento (FK)';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN id_nfe COMMENT 'Número da Nota Fiscal Eletrônica. Chave de cruzamento (FK)';
+# MAGIC
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN versao_layout COMMENT 'Versão do leiaute';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN num_uf COMMENT 'Código da UF do emitente do Documento Fiscal';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN cod_chave_acesso COMMENT 'Código Numérico que compõe a Chave de Acesso';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN cod_nat_operacao COMMENT 'Descrição da Natureza da Operação';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN ind_forma_pag COMMENT 'Indicador da forma de pagamento';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN cod_modelo_fiscal COMMENT 'Código do Modelo do Documento Fiscal';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN num_serie COMMENT 'Série do Documento Fiscal';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN num_doc_fiscal COMMENT 'Número do Documento Fiscal';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN dt_emissao COMMENT 'Data e hora de emissão do Documento Fiscal';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN dt_saida_entrada COMMENT 'Data e hora de Saída ou da Entrada da Mercadoria/Produto';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN cod_tipo_operacao COMMENT 'Tipo de Operação';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN cod_mun_fato_gerador COMMENT 'Código do Município de Ocorrência do Fato Gerador';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN cod_tipo_impressao_danfe COMMENT 'Formato de Impressão do DANFE';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN cod_tipo_emissao_nf COMMENT 'Tipo de Emissão da NF-e';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN num_dv_chave_acesso COMMENT 'Dígito Verificador da Chave de Acesso da NF-e';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN tip_ambiente COMMENT 'Identificação do Ambiente';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN cod_finalidade_nfe COMMENT 'Finalidade de emissão da NF-e';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN ind_oper_consumidor_final COMMENT 'Indica operação com Consumidor final';
+# MAGIC ALTER TABLE nfe_ide ALTER COLUMN ind_presencao_comprador COMMENT 'Indicador de presença do comprador no estabelecimento comercial no momento da operação';
+# MAGIC
+# MAGIC
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC
-# MAGIC USE CATALOG tax;
-# MAGIC USE SCHEMA silver;
+# MAGIC USE catalog tax;
+# MAGIC USE silver;
 # MAGIC
-# MAGIC ALTER TABLE nfe ALTER COLUMN cod_nfe COMMENT 'TAG raiz da NF-e';
-# MAGIC ALTER TABLE nfe ALTER COLUMN infNFe COMMENT 'Informações da NF-e';
-# MAGIC ALTER TABLE nfe ALTER COLUMN versao_layout COMMENT 'Versão do leiaute';
-# MAGIC ALTER TABLE nfe ALTER COLUMN id_nfe COMMENT 'Identificador da TAG a ser assinada';
-# MAGIC ALTER TABLE nfe ALTER COLUMN pk_nItem COMMENT 'Regra para que a numeração do item de detalhe da NF-e
-# MAGIC seja única.';
-# MAGIC ALTER TABLE nfe ALTER COLUMN ide COMMENT 'Informações de identificação da NF-e';
-# MAGIC ALTER TABLE nfe ALTER COLUMN uf COMMENT 'Código da UF do emitente do Documento Fiscal';
-# MAGIC ALTER TABLE nfe ALTER COLUMN cod_chave_acesso COMMENT 'Código Numérico que compõe a Chave de Acesso';
-# MAGIC ALTER TABLE nfe ALTER COLUMN cod_nat_operacao COMMENT 'Descrição da Natureza da Operação';
-# MAGIC ALTER TABLE nfe ALTER COLUMN ind_forma_pag COMMENT 'Indicador da forma de pagamento';
-# MAGIC ALTER TABLE nfe ALTER COLUMN cod_modelo_fiscal COMMENT 'Código do Modelo do Documento Fiscal';
-# MAGIC ALTER TABLE nfe ALTER COLUMN num_serie COMMENT 'Série do Documento Fiscal';
-# MAGIC ALTER TABLE nfe ALTER COLUMN num_doc_fiscal COMMENT 'Número do Documento Fiscal';
-# MAGIC ALTER TABLE nfe ALTER COLUMN dt_emissao COMMENT 'Data e hora de emissão do Documento Fiscal';
-# MAGIC ALTER TABLE nfe ALTER COLUMN dt_saida_entrada COMMENT 'Data e hora de Saída ou da Entrada da Mercadoria/Produto';
-# MAGIC ALTER TABLE nfe ALTER COLUMN tip_operacao COMMENT 'Tipo de Operação';
-# MAGIC ALTER TABLE nfe ALTER COLUMN cod_mun_fato_gerador COMMENT 'Código do Município de Ocorrência do Fato Gerador';
-# MAGIC ALTER TABLE nfe ALTER COLUMN tpImp COMMENT 'Formato de Impressão do DANFE';
-# MAGIC ALTER TABLE nfe ALTER COLUMN tpEmis COMMENT 'Tipo de Emissão da NF-e';
-# MAGIC ALTER TABLE nfe ALTER COLUMN cDV COMMENT 'Dígito Verificador da Chave de Acesso da NF-e';
-# MAGIC ALTER TABLE nfe ALTER COLUMN tpAmb COMMENT 'Identificação do Ambiente';
-# MAGIC ALTER TABLE nfe ALTER COLUMN finNFe COMMENT 'Finalidade de emissão da NF-e';
-# MAGIC ALTER TABLE nfe ALTER COLUMN indFinal COMMENT 'Indica operação com Consumidor final';
-# MAGIC ALTER TABLE nfe ALTER COLUMN indPres COMMENT 'Indicador de presença do comprador no estabelecimento comercial no momento da operação';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit COMMENT 'Identificação do emitente da NF-e';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_cnpj COMMENT 'CNPJ do emitente';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_xnome COMMENT 'Razão Social ou Nome do emitente';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_xfant COMMENT 'Nome fantasia';
-# MAGIC ALTER TABLE nfe ALTER COLUMN enderEmit COMMENT 'Endereço do emitente';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_logradouro COMMENT 'Logradouro do emitente';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_nro COMMENT 'Número';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_compl COMMENT 'Complemento';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_bairro COMMENT 'Bairro';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_cmun COMMENT 'Código do município';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_xmun COMMENT 'Nome do município';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_uf COMMENT 'Sigla da UF';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_cep COMMENT 'Código do CEP';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_cpais COMMENT 'Código do País';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_xpais COMMENT 'Nome do País';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_end_fone COMMENT 'Telefone';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_insc_estadual COMMENT 'Inscrição Estadual do Emitente';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_insc_estadual_st COMMENT 'IE do Substituto Tributário';
-# MAGIC ALTER TABLE nfe ALTER COLUMN emit_insc_municipal COMMENT 'Inscrição Municipal do Prestador de Serviço';
-# MAGIC ALTER TABLE nfe ALTER COLUMN cod_cnae COMMENT 'CNAE fiscal';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_cnpj COMMENT 'CNPJ do emitente';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_nome_emitente COMMENT 'Razão Social ou Nome do emitente';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_nome_fantasia COMMENT 'Nome fantasia';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_logradouro COMMENT 'Logradouro do emitente';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_nro COMMENT 'Número';
+# MAGIC -- ALTER TABLE nfe_emit ALTER COLUMN emit_end_complemento COMMENT 'Complemento';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_bairro COMMENT 'Bairro';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_cod_municipio COMMENT 'Código do município';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_municipio COMMENT 'Nome do município';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_uf COMMENT 'Sigla da UF';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_cep COMMENT 'Código do CEP';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_cod_pais COMMENT 'Código do País';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_pais COMMENT 'Nome do País';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_end_fone COMMENT 'Telefone';
+# MAGIC ALTER TABLE nfe_emit ALTER COLUMN emit_insc_estadual COMMENT 'Inscrição Estadual do Emitente';
+# MAGIC -- ALTER TABLE nfe_emit ALTER COLUMN emit_insc_estadual_st COMMENT 'IE do Substituto Tributário';
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC USE catalog tax;
+# MAGIC USE silver;
+# MAGIC
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_cnpj COMMENT 'CNPJ do destinatário';
+# MAGIC -- ALTER TABLE nfe_dest ALTER COLUMN dest_cpf COMMENT 'CPF do destinatário';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_nome_destinatario COMMENT 'Razão Social ou nome do destinatário';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_logradouro COMMENT 'Logradouro';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_num COMMENT 'Número';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_complemento COMMENT 'Complemento';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_bairro COMMENT 'Bairro';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_cod_municipio COMMENT 'Código do município';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_municipio COMMENT 'Nome do município';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_uf COMMENT 'Sigla da UF';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_cep COMMENT 'Código do CEP';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_cod_pais COMMENT 'Código do País';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_pais COMMENT 'Nome do País';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_end_fone COMMENT 'Telefone';
+# MAGIC ALTER TABLE nfe_dest ALTER COLUMN dest_insc_estadual COMMENT 'Inscrição Estadual do Destinatário';
+# MAGIC -- ALTER TABLE nfe_dest ALTER COLUMN dest_insc_suframa COMMENT 'Inscrição na SUFRAMA';
+# MAGIC -- ALTER TABLE nfe_dest ALTER COLUMN dest_insc_municipal COMMENT 'Inscrição Municipal do Tomador do Serviço';
